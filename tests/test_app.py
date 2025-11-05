f1d44b602d5c624cdcc33ed84f569683baa9646b
Filipe <filipeaugustto22@gmail.com>
2025-11-05 10:48:40 -0300

    adicionando schema de mensagem e testes

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,3 +1,5 @@
+from http import HTTPStatus
+
 from fastapi.testclient import TestClient
 
 from fast_zero.app import app
@@ -9,3 +11,13 @@ def test_root_deve_retornar_hello_world():
     response = client.get('/')
 
     assert response.json() == {'message': 'Olá mundo!'}
+    assert response.status_code == HTTPStatus.OK
+
+
+def test_show_hello_world():
+    client = TestClient(app)
+
+    response = client.get('/hello-world')
+
+    assert response.status_code == HTTPStatus.OK
+    assert '<h1> Olá Mundo </h1>' in response.text
